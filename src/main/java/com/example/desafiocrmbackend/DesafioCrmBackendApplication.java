89e0167e94140d45c8b044dad82d9e15e7e11d16
Claudio <contatojhonfk@gmail.com>
2024-09-03 10:08:45 -0300

    initial commit

---
+++ src/main/java/com/example/desafiocrmbackend/DesafioCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafiocrmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioCrmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/desafiocrmbackend/DesafioCrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafiocrmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
